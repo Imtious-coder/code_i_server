5e853bef51fb182bdaecc6c26357e5aa4947d8aa
unknown <iarafat386@gmail.com>
2021-05-03 13:44:51 +0600

    MongoDB connected succesfully

--- index.js
+++ index.js
@@ -2,9 +2,26 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://code_1:codei@cluster0.vx9y5.mongodb.net/code_i?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(cors());
 
+
+client.connect(err => {
+    const collection = client.db("code_i").collection("info");
+    console.log("database conected successfully");
+
+    app.post("/add", (req, res) => {
+        const info = req.body;
+        collection.insertOne(info)
+        .then(result => {
+            console.log(result);
+        })
+    })
+  });
+  
 
 
 
